fbc93dd6ba1c06a1bcb62e3941ba7f04ed658fad
Saul Shanabrook <s.shanabrook@gmail.com>
2019-09-09 13:24:56 -0400

    Add empty to function

--- metadsl_core/function.py
+++ metadsl_core/function.py
@@ -32,6 +32,10 @@ class FunctionZero(Expression, typing.Generic[T]):
         return cls.create(fn.__name__, fn())
 
     @expression
+    def to_fn(self) -> typing.Callable[[], T]:
+        ...
+
+    @expression
     def __call__(self) -> T:
         ...
 
@@ -54,6 +58,10 @@ class FunctionOne(Expression, typing.Generic[T, U]):
     @classmethod
     def from_fn(cls, fn: typing.Callable[[T], U]) -> FunctionOne[T, U]:
         return cls.create(fn.__name__, Abstraction.from_fn(fn))
+
+    @expression
+    def to_fn(self) -> typing.Callable[[T], U]:
+        ...
 
     @expression
     @classmethod
@@ -102,6 +110,10 @@ class FunctionTwo(Expression, typing.Generic[T, U, V]):
             return Abstraction.from_fn(inner_inner)
 
         return cls.create(fn.__name__, Abstraction.from_fn(inner))
+
+    @expression
+    def to_fn(self) -> typing.Callable[[T, U], V]:
+        ...
 
     @expression
     @classmethod
@@ -164,6 +176,10 @@ class FunctionThree(Expression, typing.Generic[T, U, V, X]):
             return inner_inner
 
         return cls.create(fn.__name__, inner)
+
+    @expression
+    def to_fn(self) -> typing.Callable[[T, U, V], X]:
+        ...
 
     @expression
     @classmethod
