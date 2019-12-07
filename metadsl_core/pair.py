7d9a234392a01d65ff86cb20960a1a1ddce3d7b8
Saul Shanabrook <s.shanabrook@gmail.com>
2019-12-07 16:38:25 -0600

    Add pair set

--- metadsl_core/pair.py
+++ metadsl_core/pair.py
@@ -23,6 +23,14 @@ class Pair(Expression, typing.Generic[T, U]):
         ...
 
     @expression
+    def set_left(self, left: T) -> Pair[T, U]:
+        ...
+
+    @expression
+    def set_right(self, right: U) -> Pair[T, U]:
+        ...
+
+    @expression
     @classmethod
     def create(cls, left: T, right: U) -> Pair[T, U]:
         ...
@@ -50,3 +58,21 @@ def pair_right(l: T, r: U) -> R[U]:
     20
     """
     return Pair.create(l, r).right, r
+
+
+@register  # type: ignore
+@rule
+def pair_set(l: T, r: U, new_l: T, new_r: U) -> R[Pair[T, U]]:
+    """
+    >>> execute(Pair.create(10, 20).set_left(5).left)
+    5
+    >>> execute(Pair.create(10, 20).set_left(5).right)
+    20
+    >>> execute(Pair.create(10, 20).set_right(5).left)
+    10
+    >>> execute(Pair.create(10, 20).set_right(5).right)
+    5
+    """
+    yield Pair.create(l, r).set_left(new_l), Pair.create(new_l, r)
+    yield Pair.create(l, r).set_right(new_r), Pair.create(l, new_r)
+
