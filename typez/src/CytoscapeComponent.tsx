b4ef0bc0f14cf5e3de21ab8c6f375e9eedec7dad
Saul Shanabrook <s.shanabrook@gmail.com>
2020-01-08 14:20:13 -0500

    Fix layout

--- typez/src/CytoscapeComponent.tsx
+++ typez/src/CytoscapeComponent.tsx
@@ -122,8 +122,8 @@ const layout = {
     // Enums use the name of the enum e.g.
     // searchOrder: "BFS",
     zoomToFit: true,
-    algorithm: "layered"
-    // direction: "down"
+    algorithm: "conn.gmf.layouter.Draw2D"
+    // direction: "DOWN"
   }
   // priority: function( edge ){ return null; }, // Edges with a non-nil value are skipped when geedy edge cycle breaking is enabled
 };
