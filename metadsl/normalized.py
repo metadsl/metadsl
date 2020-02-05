aec3939b685423682f173f6f2bd12a010851b865
Saul Shanabrook <s.shanabrook@gmail.com>
2020-02-05 13:42:47 -0500

    Remove faulty id gen logic and add new duplicate node test nb

--- metadsl/normalized.py
+++ metadsl/normalized.py
@@ -88,12 +88,9 @@ class NormalizedExpressions:
     expressions: collections.OrderedDict[
         Hash, NormalizedExpression
     ] = dataclasses.field(default_factory=collections.OrderedDict)
-    ids: typing.Dict[ID, Hash] = dataclasses.field(default_factory=dict)
 
-    # keep track of all the IDs we have assigned to each hash previously
-    # so that any hash which has had a previous ID assigned in some older
-    # cycle will keep that same ID.
-    asigned_ids: typing.Dict[Hash, ID] = dataclasses.field(default_factory=dict)
+    # mapping of IDs to hashes for this set of expression
+    ids: typing.Dict[ID, Hash] = dataclasses.field(default_factory=dict)
 
     next_id: ID = ID(0)
     # THe first hash  added. Used when adding a new expression, so we can record the left
@@ -193,17 +190,15 @@ class NormalizedExpressions:
         if not self._first_hash:
             self._first_hash = hash
         if hash not in self.expressions:
-            if hash in self.asigned_ids:
-                id = self.asigned_ids[hash]
-            else:
-                id = self._get_new_id(prev_id)
-                self.asigned_ids[hash] = id
+            id = self._get_new_id(prev_id)
             self.expressions[hash] = NormalizedExpression(children, expr, id)
             self.ids[id] = hash
         return hash
 
     def replace(self, hash: typing.Optional[Hash], expr: object) -> None:
-        new_expressions = NormalizedExpressions(next_id=self.next_id, asigned_ids=self.asigned_ids)
+        new_expressions = NormalizedExpressions(
+            next_id=self.next_id
+        )
         if not hash:
             # If we didn't get a hash, we are replacing the root node
             root_hash = next(iter(reversed(self.expressions)))
