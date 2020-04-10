ccb51ea816249c5e355ad840d477a98cd3ff8ef2
Saul Shanabrook <s.shanabrook@gmail.com>
2020-04-10 11:08:53 -0400

    Ignore black logging

--- metadsl/logging.py
+++ metadsl/logging.py
@@ -3,5 +3,20 @@ import logging
 __all__ = ["toggle_debug_logging"]
 
 
-def toggle_debug_logging(enabled: int) -> None:
-    logging.basicConfig(level=logging.DEBUG if enabled else logging.WARNING, force=True)  # type: ignore
+def metadsl_filter(record: logging.LogRecord) -> bool:
+    """
+    Only allow through if in metadsl
+    """
+    return record.name.startswith("metadsl")
+
+
+handler = logging.StreamHandler()
+handler.addFilter(metadsl_filter)
+
+
+def toggle_debug_logging(enabled: bool) -> None:
+    logging.basicConfig(  # type: ignore
+        level=logging.DEBUG if enabled else logging.WARNING,
+        force=True,
+        handlers=[handler],
+    )
