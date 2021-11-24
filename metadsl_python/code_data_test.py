c77e4072654a7dd468fd6f0016b76a17ac6edb11
Saul Shanabrook <s.shanabrook@gmail.com>
2021-11-24 12:02:08 -0500

    Make tests run in xdist

--- metadsl_python/code_data_test.py
+++ metadsl_python/code_data_test.py
@@ -5,63 +5,52 @@ import warnings
 import pkgutil
 from importlib.abc import Loader
 from types import CodeType
-from typing import Optional, cast
-import pytest
+from typing import Iterable
 
 from .code_data import CodeData
 
 
-def safe_get_code(loader: Loader, name: str) -> Optional[CodeType]:
-    """
-    Try to get the code for a module, but if syntax is wrong, just return None
-    """
-    try:
-        return loader.get_code(name)
-    except SyntaxError:
-        return None
-
-
 # Special cases to test manually
-SAMPLE_CODE = {"variable": "a"}
-
-code_params = [
-    pytest.param(compile(str, "<str>", "exec"), id=name)
-    for name, str in SAMPLE_CODE.items()
-]
-
-# In order to test the code_data, we try to get a sample of bytecode,
-# by walking all our packages and trying to load every module.
-# Note that although this doesn't require the code to be executable,
-# `walk_packages` does require it, so this will ignore any modules
-# which raise errors on import.
-# Catch warnings so warnings raised on import are surpressed
-with warnings.catch_warnings():
-    module_infos = pkgutil.walk_packages(onerror=lambda _name: None)
-
-code_params += [
-    pytest.param(code, id=mi.name)
-    for mi in module_infos
-    if (
-        code := safe_get_code(
-            cast(Loader, mi.module_finder.find_module(mi.name)), mi.name
-        )
-    )
-]
+SAMPLE_CODE = ["a"]
 
 
-@pytest.mark.parametrize("code", code_params)
-def test_code_data(code: CodeType):
-    # Note: Make sure not to store a copy of the CodeData instance,
-    # or if you do, delete if before trying to dump it.
-    # marshalling will treat values with refernces differently sometimes
-    # and store them as "refs" to prevent cycles, so the bytes will not be ==
-    resulting_code = CodeData.from_code(code).to_code()
+def test_code_data():
+    # Instead of params, iterate in test so that:
+    # 1. the number of tests is consistant accross python versions pleasing xdist running multiple versions
+    # 2. pushing loading of all modules inside generator, so that fast samples run first
+    for code in codes():
 
-    # We start by comparing the data first and then the marshalled bytes.
-    # Compares the bytes gives us the highest assurance that our parsing
-    # was isomorphic, but also is harder to parse if there is an error.
-    assert code_to_dict(resulting_code) == code_to_dict(code)
-    assert marshal.dumps(resulting_code) == marshal.dumps(code)
+        # Note: Make sure not to store a copy of the CodeData instance,
+        # or if you do, delete if before trying to dump it.
+        # marshalling will treat values with refernces differently sometimes
+        # and store them as "refs" to prevent cycles, so the bytes will not be ==
+        resulting_code = CodeData.from_code(code).to_code()
+
+        # We start by comparing the data first and then the marshalled bytes.
+        # Compares the bytes gives us the highest assurance that our parsing
+        # was isomorphic, but also is harder to parse if there is an error.
+        assert code_to_dict(resulting_code) == code_to_dict(code)
+        assert marshal.dumps(resulting_code) == marshal.dumps(code)
+
+
+def codes() -> Iterable[CodeType]:
+    for s in SAMPLE_CODE:
+        yield compile(s, "<str>", "exec")
+
+    # In order to test the code_data, we try to get a sample of bytecode,
+    # by walking all our packages and trying to load every module.
+    # Note that although this doesn't require the code to be executable,
+    # `walk_packages` does require it, so this will ignore any modules
+    # which raise errors on import.
+    # Catch warnings so warnings raised on import are surpressed
+
+    with warnings.catch_warnings():
+        for mi in pkgutil.walk_packages(onerror=lambda _name: None):
+            loader: Loader = mi.module_finder.find_module(mi.name)
+            try:
+                yield loader.get_code(mi.name)
+            except SyntaxError:
+                continue
 
 
 def code_to_dict(code: CodeType) -> dict[str, object]:
