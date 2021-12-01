870578eec8c1d38c4803737eaf01536b2d0ef8fa
Saul Shanabrook <s.shanabrook@gmail.com>
2021-12-01 13:14:44 -0500

    Started adding each bytecode

--- metadsl_python/instruction_data.py
+++ metadsl_python/instruction_data.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Iterable, List, Optional, Tuple
+from typing import Iterable, List, Literal, Optional, Tuple, Protocol, Callable
 from itertools import chain
+from functools import partial
+from abc import abstractmethod
 
 
 def instructions_from_bytes(b: bytes) -> Iterable[InstructionData]:
     for i in range(0, len(b), 2):
-        yield InstructionData(b[i], b[i + 1])
+        bytecode = b[i]
+        value = b[i + 1]
+        yield instruction_creators[bytecode](value)
 
 
 def instructions_to_bytes(instructions: Iterable[InstructionData]) -> bytes:
@@ -17,8 +21,47 @@ def instructions_to_bytes(instructions: Iterable[InstructionData]) -> bytes:
     )
 
 
-@dataclass
-class InstructionData:
+class InstructionData(Protocol):
     opcode: int
     value: int
 
+
+@dataclass
+class UnknownInstruction:
+    opcode: int
+    value: int
+
+
+# List indexed by btecode, to make instruction data from the value
+instruction_creators: List[Callable[[int], InstructionData]] = [
+    partial(UnknownInstruction, op) for op in range(256)
+]
+
+
+class RegisterOpcode(InstructionData):
+    def __init_subclass__(cls) -> None:
+        if hasattr(cls, "opcode"):
+            instruction_creators[cls.opcode] = cls.from_value
+
+    @classmethod
+    @abstractmethod
+    def from_value(cls, value: int) -> InstructionData:
+        ...
+
+
+@dataclass  # type: ignore
+class NoValue(RegisterOpcode):
+    value: int
+
+    @classmethod
+    def from_value(cls, value: int) -> NoValue:
+        return cls(value)
+
+
+@dataclass
+class Nop(NoValue):
+    """
+    Do nothing code. Used as a placeholder by the bytecode optimizer.
+    """
+
+    opcode = 9
