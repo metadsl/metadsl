2c776a9f121f5db99c3b8fd2dc1b382b36a22793
Saul Shanabrook <s.shanabrook@gmail.com>
2021-12-08 13:33:30 -0500

    Don't compare marshalled

--- metadsl_python/code_data_test.py
+++ metadsl_python/code_data_test.py
@@ -1,19 +1,17 @@
 from __future__ import annotations
 
-import marshal
 import warnings
 import pkgutil
 from importlib.abc import Loader
 from types import CodeType
 from typing import Iterable
-from hypothesis import example, given
-import pytest
+from hypothesis import example, given, settings, HealthCheck
 import hypothesmith
 
 from .code_data import CodeData
 
 
-def test_code_data_from_modules():
+def test_modules():
     # Instead of params, iterate in test so that:
     # 1. the number of tests is consistant accross python versions pleasing xdist running multiple versions
     # 2. pushing loading of all modules inside generator, so that fast samples run first
@@ -22,15 +20,15 @@ def test_code_data_from_modules():
 
 
 @given(source_code=hypothesmith.from_node())
+@settings(suppress_health_check=(HealthCheck.filter_too_much,))
 @example("a")
 @example("class A: pass\nclass A: pass\n")
-def test_code_data_generated(source_code):
+def test_generated(source_code):
     code = compile(source_code, "<string>", "exec")
     verify_code(code)
 
 
 def module_codes() -> Iterable[CodeType]:
-
     # In order to test the code_data, we try to get a sample of bytecode,
     # by walking all our packages and trying to load every module.
     # Note that although this doesn't require the code to be executable,
@@ -52,32 +50,5 @@ def module_codes() -> Iterable[CodeType]:
 
 
 def verify_code(code: CodeType) -> None:
-    # Note: Make sure not to store a copy of the CodeData instance,
-    # or if you do, delete if before trying to dump it.
-    # marshalling will treat values with refernces differently sometimes
-    # and store them as "refs" to prevent cycles, so the bytes will not be ==
     resulting_code = CodeData.from_code(code).to_code()
-    gc.collect()
-
-    # We start by comparing the data first and then the marshalled bytes.
-    # Compares the bytes gives us the highest assurance that our parsing
-    # was isomorphic, but also is harder to parse if there is an error.
-    assert code_to_dict(resulting_code) == code_to_dict(code)
-
-    assert marshal.dumps(resulting_code) == marshal.dumps(code)
-
-
-def code_to_dict(code: CodeType) -> dict[str, object]:
-    """
-    Converts a code object to a dict for testing
-    """
-    return {
-        name: getattr(code, name)
-        for name in dir(code)
-        if name.startswith("co_")
-        # Don't compare generated co_lines iterator returned in Python 3.10
-        # When co_lntob is removed in 3.12, we need to figured out how to adapt.
-        # TODO: look at how co_lines works and make sure we can duplicate logic for mapping
-        # https://docs.python.org/3/whatsnew/3.10.html?highlight=co_lines#pep-626-precise-line-numbers-for-debugging-and-other-tools
-        and name != "co_lines"
-    }
+    assert code == resulting_code
