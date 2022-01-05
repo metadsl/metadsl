f1ee65555fec0b33d12baa7d28355e15d4dc2e76
Saul Shanabrook <s.shanabrook@gmail.com>
2022-01-05 17:00:59 -0500

    Only store opname in instruction

--- metadsl_python/instruction_data.py
+++ metadsl_python/instruction_data.py
@@ -13,19 +13,15 @@ import sys
 
 
 def instructions_from_bytes(b: bytes) -> Iterable[InstructionData]:
-    extended_arg = 0
-    n_extended_args = 0
+    args = []
     for i in range(0, len(b), 2):
-        # TODO: Longer value bytes
         opcode = b[i]
-        arg = b[i + 1] | extended_arg
+        args.append(b[i + 1])
+        # Build up our list of args whenever we have an extended arg
         if opcode == dis.EXTENDED_ARG:
-            extended_arg = arg << 8
-            n_extended_args += 1
-        else:
-            extended_arg = 0
-            yield InstructionData(i, n_extended_args, opcode, arg)
-            n_extended_args = 0
+            continue
+        yield InstructionData.from_bytes(offset=i, args=args, opcode=opcode)
+        args = []
 
 
 def instructions_to_bytes(instructions: Iterable[InstructionData]) -> bytes:
@@ -41,54 +37,80 @@ ATLEAST_310 = sys.version_info >= (3, 10)
 
 @dataclass
 class InstructionData:
-    # TODO: Don't store offset or jump target offeset, instead store block number
     # The bytes offset of the instruction
     offset: int
-    # The number of extended args
+
+    # The name of the instruction
+    name: str
+
+    # The number of args, if it differs form the instrsize
     # Note: in Python >= 3.10 we can calculute this from the instruction size,
     # using `instrsize`, but in python < 3.10, sometimes instructions are prefixed
     # with extended args with value 0 (not sure why or how), so we need to save
     # the value manually to recreate the instructions
-    n_extended_args: int = field(repr=False)
+    n_args_override: Optional[int] = field(repr=False)
 
-    opcode: int
+    # The integer value of the arg
     arg: int
 
     # The bytes offset of the jump target, if it does jump.
-    jump_target_offset: Optional[int] = field(init=False)
+    jump_target_offset: Optional[int]
 
-    name: str = field(init=False)
+    @classmethod
+    def from_bytes(cls, offset: int, opcode: int, args: list[int]) -> InstructionData:
 
-    def __post_init__(self):
-        # The total numver of required args should be at least big enough to hold the arg
-        assert self.n_extended_args + 1 >= instrsize(self.arg)
+        name = dis.opname[opcode]
+
+        # Compute arg by starting with highest byte and working down
+        arg, *rest_args = args
+        for next_arg in rest_args:
+            arg = (arg << 8) | next_arg
 
         # Copied from dis.findlabels
-        self.jump_target_offset = (
-            (2 if ATLEAST_310 else 1) * self.arg
-            if self.opcode in dis.hasjabs
-            else self.offset + 2 + ((2 if ATLEAST_310 else 1) * self.arg)
-            if self.opcode in dis.hasjrel
+        jump_target_offset = (
+            (2 if ATLEAST_310 else 1) * arg
+            if opcode in dis.hasjabs
+            else offset + 2 + ((2 if ATLEAST_310 else 1) * arg)
+            if opcode in dis.hasjrel
             else None
         )
 
-        self.name = dis.opname[self.opcode]
+        min_n_args = instrsize(arg)
+        n_args = len(args)
+        # The number of args should be at least the minimum
+        assert n_args >= min_n_args
+        n_args_override = None if n_args == min_n_args else n_args
+
+        return cls(
+            offset=offset,
+            name=name,
+            arg=arg,
+            jump_target_offset=jump_target_offset,
+            n_args_override=n_args_override,
+        )
 
     def bytes(self) -> Iterable[int]:
         # Duplicate semantics of write_op_arg
         # to produce the the right number of extended arguments
         # https://github.com/python/cpython/blob/b2e5794870eb4728ddfaafc0f79a40299576434f/Python/wordcode_helpers.h#L22-L44
-        for i in range(self.n_extended_args, -1, -1):
-            yield self.opcode if i == 0 else dis.EXTENDED_ARG
+        for i in reversed(range(self.n_args())):
+            yield self.opcode() if i == 0 else dis.EXTENDED_ARG
             yield (self.arg >> (8 * i)) & 0xFF
+
+    def n_args(self) -> int:
+        if self.n_args_override is not None:
+            return self.n_args_override
+        return instrsize(self.arg)
+
+    def opcode(self) -> int:
+        return dis.opmap[self.name]
 
     def offsets(self) -> Iterable[int]:
         """
         Returns all the offsets for the instruction, including those for the extended
         args that appear before it
         """
-        yield self.offset
-        for i in range(self.n_extended_args):
+        for i in range(self.n_args()):
             yield self.offset - (i * 2)
 
 
