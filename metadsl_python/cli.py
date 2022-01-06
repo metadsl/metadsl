831e956ec03d9fa6209a2513a1fdfb73434415a1
Saul Shanabrook <s.shanabrook@gmail.com>
2022-01-06 11:19:37 -0500

    Toggle source code via flag

--- metadsl_python/cli.py
+++ metadsl_python/cli.py
@@ -16,12 +16,20 @@ parser.add_argument(
 )
 parser.add_argument("-m", type=str, help="python library", metavar="mod")
 parser.add_argument("--dis", action="store_true", help="print Python's dis analysis")
+parser.add_argument("--source", action="store_true", help="print the source code")
 
 if __name__ == "__main__":
     from rich.console import Console
 
     args = parser.parse_args()
-    file, cmd, mod, eval_, show_dis = args.file, args.c, args.m, args.e, args.dis
+    file, cmd, mod, eval_, show_dis, show_source = (
+        args.file,
+        args.c,
+        args.m,
+        args.e,
+        args.dis,
+        args.source,
+    )
 
     if len(list(filter(None, [file, cmd, mod, eval_]))) != 1:
         parser.error("Must specify exactly one of file, cmd, eval, or mod")
@@ -47,7 +55,7 @@ if __name__ == "__main__":
         source = spec.loader.get_source(mod)  # type: ignore
         assert code
 
-    if source is not None:
+    if show_source and source is not None:
         console.print(Syntax(source, "python", line_numbers=True))
     if show_dis:
         dis.dis(code)
